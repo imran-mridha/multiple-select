f4072d64b65bdb85006d77515b493ccca947c4b5
imran-mridha <mdimranmridha.pro@gmail.com>
2023-02-25 00:31:38 +0600

    Multiple select

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+function MultiSelect() {
+  const options = [
+    { value: 'option1', label: 'Option 1' },
+    { value: 'option2', label: 'Option 2' },
+    { value: 'option3', label: 'Option 3' }
+  ];
+
+  const [selectedOptions, setSelectedOptions] = useState([]);
+console.log(selectedOptions);
+  const handleChange = (event) => {
+    const value = event.target.value;
+
+    if (selectedOptions.includes(value)) {
+      setSelectedOptions(selectedOptions.filter(option => option !== value));
+    } else {
+      setSelectedOptions([...selectedOptions, value]);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <select value={selectedOptions.join(',')} onChange={handleChange} >
+          <option value="" disabled hidden>Select options...</option>
+          {options.map(option => (
+            <option key={option.value} value={option.value}>
+              {option.label}
+            </option>
+          ))}
+        </select>
+        <div>
+        {selectedOptions.join(',')}
+        </div>
+      </div>
     </div>
   );
 }
 
-export default App;
+export default MultiSelect;
